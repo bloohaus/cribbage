142df9c06ff69254ed14a9c0f2d116585fb5afca
dave <dfstearns+git@gmail.com>
2018-07-05 09:54:09 -0700

    working. removed all logging.

--- cribbage.c
+++ cribbage.c
@@ -7,7 +7,7 @@
 #define HANDLENGTH 6
 #define UPINDEX 22
 
-int fifteenPoints(deck hand, int sum, int position, card *set);
+int fifteenPoints(deck hand, int sum, int position);
 int runPoints(deck hand);
 int pairPoints(deck hand);
 int cardValue(card c);
@@ -48,19 +48,12 @@ int main(int argc, char * argv[]){
 	scoreHand = joinDecks(hands[0], upCard);
 	sortDeck(&scoreHand);
 	printDeck(scoreHand);
-    
-	card emptySet[6];
-	emptySet[0].value = 0;
 
     pPoints = pairPoints(scoreHand);
     printf("there were %d pair points in the first hand.\n", pPoints);
-    for (i = 0; i < scoreHand.len; i++){
 
-		if (cardValue(scoreHand.cards[i]) > 7){
-			break;
-		}
-    	fPoints += fifteenPoints(scoreHand, 0, i, emptySet);
-    }
+    fPoints = fifteenPoints(scoreHand, 0, 0);
+
     printf("there were %d fifteen points in the first hand.\n", fPoints);
 }
 
@@ -72,83 +65,21 @@ int cardValue(card c){
     }
 }
 
-int setLen(card *set){
-	int i;
-	i = 0;
-
-	while (set[i].value != 0){
-		i++;
-	}
-	return i;
-}
-
-void addCardtoSet(card c, card *set){
-	
-	int i;
-	i = setLen(set);
-
-	set[i] = c;
-	set[i + 1].value = 0;
-}
-
-card cardPop(card *set){
-	int i;
-	card c;
-	i = setLen(set);
-	c = set[i - 1];
-	set[i - 1].value = 0;
-
-	return c;
-}
-
-void printSet(card *set){
-	int i, j;
-	j = setLen(set);
-	for (i = 0; i < j; i++){
-		printCard(set[i]);
-	}
-}
-
-int fifteenPoints(deck hand, int sum, int position, card* set){
+int fifteenPoints(deck hand, int sum, int position){
 	int i, j;
 	int points, workingSum;
-	card homeSet[6];
-
-	/* copy cards from caller to function frame */
-	j = setLen(set);
-	for (i = 0; i < j; i++){
-		homeSet[i] = set[i];
-	}
-
-	homeSet[i].value = 0;
 
 	points = 0;
 
 	for (i = position; i < hand.len; i++){
 		workingSum = sum + cardValue(hand.cards[i]);
-		addCardtoSet(hand.cards[i], homeSet);
-
 		if (workingSum > 15){
-			printSet(homeSet);
-			printf("adds up to %d\n", workingSum);
-			printf("is higher than 15.\n");
 			break;
 		} else if (workingSum == 15){
-			printSet(homeSet);
-			printf("adds up to %d\n", workingSum);
-			printf("is 15. 2 points.\n");
 			points += 2;
 		} else if (workingSum < 15){
-					printSet(homeSet);
-		printf("adds up to %d\n", workingSum);
-		printSet(homeSet);
-		printf("adds up to %d\n", workingSum);
-		printf("is less than 15. let's call fifteenPoints(hand, workingSum: %d, position: %d, set)\n", workingSum, j);
-		points += fifteenPoints(hand, workingSum, i + 1, homeSet);
-			
-			
+		points += fifteenPoints(hand, workingSum, i + 1);
 		}
-		cardPop(homeSet);
 	}
     return points;
 }
